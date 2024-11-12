3663a853feb4af455cb2aa14a63bfa5a32b8af9e
kauanvitorjoaocaua <163539651+kauanvitorjoaocaua@users.noreply.github.com>
2024-11-12 08:47:35 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+function criarcartão(categoria,pergunta,resposta){
+    let container = document.getElementById( 'container')
+    let cartão = document.createElement('article')
+    cartão.className = 'cartão'
+cartão.innerHTML = `
+<div class="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+</div>
+<div class="cartao_conteudo_resposta">
+   <p>${resposta}</p>
+       </div>
+</div>  `
+container.appendchild(cartão)
+}
+let respostaEstaVisivel = false
+function viracartao(){
+    respostaEstaVisivel = !respostaEstaVisive
+    cartão.classlist.toogle('click',viracartao)
+}
+cartão.aDDeventlistener('click',viracartao)
